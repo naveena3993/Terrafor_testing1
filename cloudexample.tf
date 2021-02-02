a0767765869620fc95b4665215b093f82870cb12
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-02-02 20:45:23 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
     tenant_id       =   var.tenant_id
+    skip_provider_registration = true
     
     features {
     key_vault {
