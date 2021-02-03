6c40e28c63bf27ef019ab31c1893b0db44111513
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-02-03 22:31:39 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-    version = "<=2.26"
+provider "azurerm" {
+    version = ">=2.26"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
